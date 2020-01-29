7cbdcaf20770120db184f7db53efbda2773c0f75
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-01-29 13:44:39 -0500

    created functions to populate tweets in the DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,86 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+    },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+// This function can be responsible for taking in an array of tweet objects and then appending each one to the #tweets-container. 
+// In order to do this, the renderTweets will need to leverage the createTweetElement function you wrote earlier by passing the
+// tweet object to it, then using the returned jQuery object by appending it to the #tweets-container section.
+
+const renderTweets = function() {
+  let $tweetContainer = $('#tweet-container');
+  // loops through tweets
+  for (let tweets in tweetData) {
+  // calls createTweetElement for each tweet
+    const tweet = createTweetElement(tweets);
+  // takes return value and appends it to the tweets container
+    $tweetContainer.append(tweet)
+  }
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = $('<article>').addClass('tweet');
+
+  // Define the header and all it's descendants then append to $tweet
+  let $header = $('<header>');
+  let $div = $('<div>');
+  let $avatar = $('<img>').addClass('avatar').text(tweetData.user.avatars);
+  let $user = $('<span>').text(tweetData.user.name);
+  let $tweeterHandle = $('<span>').addClass('handle').text(tweetData.user.handle);
+
+  $div
+      .append($avatar)
+      .append($user);
+
+  $header
+        .append($div)
+        .append($tweeterHandle)
+        .appendTo($tweet);
+  
+// Define the text area then append to $tweet
+  let $tweetTextMessage = $('<p>').text(tweetData.content.text)
+
+  $tweetTextMessage
+        .appendTo($tweet);
+
+// Define the footer and all it's descendants then append to $tweet
+  let $footer = $('<footer>');
+  let $timestamp = $('<span>').text(tweetData['created_at']);
+  
+  let $iconSection = $('<div>');
+  let $flagIcon = $('<img>').addClass('flag-icon');
+  let $shareIcon = $('<img>').addClass('share-icon');
+  let $heartIcon = $('<img>').addClass('heart-icon');
+
+  $iconSection
+        .append($flagIcon)
+        .append($shareIcon)
+        .append($heartIcon);
+
+  $footer
+        .append($timestamp)
+        .append($iconSection)
+        .appendTo($tweet);
+
+  return $tweet;
+}
+
+// Will need to pass a callback that has renderTweets inside of it being called
+// Will need to change the for loop to work for an array
+$(document).ready(renderTweets);
+
+// Test / driver code (temporary)
+// const $tweet = createTweetElement(tweetData);
+// console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
