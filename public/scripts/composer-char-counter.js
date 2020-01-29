15cf17184e0f01b0c2443b6f1e95354078effddf
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-01-28 22:55:36 -0500

    Merge branch 'feature/char-counter'

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,8 @@ $(document).ready(() => {
     $('#counter').text(140 - $(this).val().length);
     if ($('#counter').text() < 0) {
       $('#counter').addClass('invalid-char-count');
+    } else if ($('#counter').text() >= 0) {
+      $('#counter').removeClass('invalid-char-count');
     }
   });
 });
