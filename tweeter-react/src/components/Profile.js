88f0dac2fadc451296692b51836e5ec8ef684ad0
Gab Richard <g.elodierichard@gmail.com>
2020-04-21 14:12:33 -0400

    create Profile component and bring in html to render

---
+++ tweeter-react/src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Profile.css';
+
+export const Profile = () => {
+
+  return (
+    <header>
+      <img id="avatar" src="/images/profile-hex.png"/> 
+      <br/>
+      <h2 id="user-id"><strong>Gab </strong>Richard</h2>
+    </header>
+  );
+};
